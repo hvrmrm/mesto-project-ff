c35cc1926949070496488242e62b4922e8c9afca
Karina Sherstneva <karinasherstneva.05@gmail.com>
2025-05-15 18:46:39 +0500

    исправления после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,30 @@
-const cardList = document.querySelector(".places__list");
+const cardsContainer = document.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 
-function createCardElement({ name, link }) {
-    const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 
+function createCardElement({ name, link }, deleteCard) {
+    const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+    
     const title = cardElement.querySelector(".card__title");
     title.textContent = name;
-
+    
     const cardImage = cardElement.querySelector(".card__image");
     cardImage.src = link;
     cardImage.alt = name;
 
-    const deleteButton = cardElement.querySelector(".card__delete-button");
-    deleteButton.addEventListener("click", () => cardElement.remove());
+    const deleteButton = cardElement.querySelector(".card__delete-button"); 
+    deleteButton.addEventListener("click", () => deleteCard(cardElement)); 
 
     return cardElement;
 }
 
 function renderCards(cards) {
     cards.forEach(cardData => {
-        const cardElement = createCardElement(cardData);
-        cardList.append(cardElement);
+        const cardElement = createCardElement(cardData, deleteCard);
+        cardsContainer.append(cardElement);
     });
 }
 
